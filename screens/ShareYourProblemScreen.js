a8f0e823f46c7352b22ea6465923ae27d509ce5c
NairritaMohan <nairrita.whitehatjr@gmail.com>
2020-10-30 11:26:51 +0530

    Added picker component

--- screens/ShareYourProblemScreen.js
+++ screens/ShareYourProblemScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {TouchableOpacity, Text, View, TextInput, StyleSheet, FlatList, Image} from 'react-native';
+import {TouchableOpacity, Text, View, TextInput, StyleSheet, FlatList, Image,Picker} from 'react-native';
 import {Badge, Icon, Header} from 'react-native-elements';
 import firebase from 'firebase';
 import db from '../config';
@@ -7,31 +7,36 @@ import {RFValue} from 'react-native-responsive-fontsize';
 import { Dropdown } from 'react-native-material-dropdown';
 
 export default class ShareYourProblemScreen extends React.Component {
-constructor(){
-    super()
-    this.state = {
-        userName : '',
-        subject : '',
-        userProblem : '',
+    state = {user: ''}
+    updateUser = (user) => {
+       this.setState({ user: user })
     }
-}
 
-submitProblem=(userName,subject,userProblem)=>{
-
-}
 
     render(){
+        
         return(
             <View style = {styles.container}>
-                 <Header 
+
+
+                 {/* <Header 
                 backgroundColor = {"#00adb5"}
                 containerStyle = {styles.header}
                 centerComponent = {{
                     text:"Share Your Problem",
-                    style : {flex:1,color:"#fff",fontWeight:'bold', fontStyle:'italic', fontSize:RFValue(30)}
+                    style : {color:"#fff",fontWeight:'bold', fontStyle:'italic', fontSize:RFValue(30)}
                 }}
-                />
-                <TextInput 
+                />  */}
+                <View>
+            <Picker selectedValue = {this.state.user} onValueChange = {this.updateUser}>
+               <Picker.Item label = "Share problem" value = "ShareProblem" />
+               <Picker.Item label = "Driving License" value = "Driving" />
+               <Picker.Item label = "Pan Card" value = "Pan " />
+            </Picker>
+            <Text style = {styles.text}>{this.state.user}</Text>
+         </View>
+                
+                {/* <TextInput 
                 style = {styles.inputBox}
                 placeholder = {"Your Name*"}
                 placeholderTextColor = {'black'}
@@ -44,7 +49,7 @@ submitProblem=(userName,subject,userProblem)=>{
                     })
                 }}
                 />
-                <TextInput 
+                 <TextInput 
                 style = {styles.inputBox}
                 placeholder = {"Subject*"}
                 placeholderTextColor = {'black'}
@@ -56,7 +61,8 @@ submitProblem=(userName,subject,userProblem)=>{
                         subject:text
                     })
                 }}
-                />
+                /> 
+                    
                 <TextInput 
                 style = {styles.inputBox2}
                 placeholder = {"Your Problem*"}
@@ -70,7 +76,7 @@ submitProblem=(userName,subject,userProblem)=>{
                         userProblem:text
                     })
                 }}
-                />
+                />  */}
                 <TouchableOpacity 
                 style = {styles.button}
                 onPress={()=>{
@@ -84,11 +90,11 @@ submitProblem=(userName,subject,userProblem)=>{
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex:1,
-        backgroundColor:"#222831",
-        alignItems:'center',
-    },
+    // container : {
+    //     flex:1,
+    //     //backgroundColor:"#222831",
+    //     alignItems:'center',
+    // },
     button : {
         backgroundColor : "#00adb5",
         borderWidth:RFValue(2),
@@ -97,35 +103,40 @@ const styles = StyleSheet.create({
         alignItems:'center',
         marginTop:RFValue(20),
         borderRadius:RFValue(5),
-    },
-    buttonText:{
-        fontWeight:'bold',
-        color:"#fff",
-        fontSize:RFValue(20),
-        fontStyle:'italic',
-        alignItems:'center',
-    },
-    inputBox:{
-        backgroundColor : '#00adb5',
-        borderColor:'#eeeeee',
-        borderRadius:RFValue(5),
-        borderWidth:RFValue(2),
-        width:RFValue(300),
-        height:RFValue(50),
-        marginTop:RFValue(20),
-    },
-    inputBox2:{
-        backgroundColor : '#00adb5',
-        borderColor:'#eeeeee',
-        borderRadius:RFValue(5),
-        borderWidth:RFValue(2),
-        width:RFValue(300),
-        height:RFValue(150),
-        marginTop:RFValue(20),
-    },
-    header : {
-        marginTop:-15,
-        flex:0.1,
-        width:RFValue(500)
-    },
+     },
+    // buttonText:{
+    //     fontWeight:'bold',
+    //     color:"#fff",
+    //     fontSize:RFValue(20),
+    //     fontStyle:'italic',
+    //     alignItems:'center',
+    // },
+    // inputBox:{
+    //     backgroundColor : '#00adb5',
+    //     borderColor:'#eeeeee',
+    //     borderRadius:RFValue(5),
+    //     borderWidth:RFValue(2),
+    //     width:RFValue(300),
+    //     height:RFValue(50),
+    //     marginTop:RFValue(20),
+    // },
+    // inputBox2:{
+    //     backgroundColor : '#00adb5',
+    //     borderColor:'#eeeeee',
+    //     borderRadius:RFValue(5),
+    //     borderWidth:RFValue(2),
+    //     width:RFValue(300),
+    //     height:RFValue(150),
+    //     marginTop:RFValue(20),
+    // },
+    // header : {
+    //     marginTop:-15,
+    //     flex:0.1,
+    //     width:RFValue(500)
+    // },
+    text: {
+        fontSize: 30,
+        alignSelf: 'center',
+        color: 'red'
+     }
 })
